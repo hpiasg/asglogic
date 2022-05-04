cfbea634c778e5c562138571521796fcda719ad1
Norman Kluge <norman.kluge@hpi.de>
2022-05-04 17:54:30 +0200

    move from javax namespace to jakarta

--- src/main/java/de/uni_potsdam/hpi/asg/logictool/io/Config.java
+++ src/main/java/de/uni_potsdam/hpi/asg/logictool/io/Config.java
@@ -19,10 +19,10 @@ package de.uni_potsdam.hpi.asg.logictool.io;
  * along with ASGlogic.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "logicconfig")
 @XmlAccessorType(XmlAccessType.NONE)
